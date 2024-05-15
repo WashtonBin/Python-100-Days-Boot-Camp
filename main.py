e4028bc1c9aec1d9da14f14345a7fd0cae1492c7
WashtonBin <154098927+WashtonBin@users.noreply.github.com>
2024-05-15 05:33:43 +0000

    check_number  return attempts

--- main.py
+++ main.py
@@ -10,39 +10,58 @@
 
 from art import logo
 import random
+from replit import clear
+
+#check number if it is correct or not and print the result
+def check_number(number, guess, attempts):
+  if number > guess:
+    print("Too low.")
+    return attempts - 1
+  elif number < guess:
+    print("Too high.")
+    return attempts - 1
+  else:
+    print(f"You got it! The answer was {number}.")
+    return -1
 
 def play_game():
   print(logo)
   print("Welcome to the Number Guessing Game!")
   print("I'm thinking of a number between 1 and 100.")
-  number = random.randint(1, 101)
+  #set guess number 1 - 100
+  number = random.randint(1, 10)
   
   difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+  #set difficulty level
   if difficulty == "easy":
     attempts = 10
   elif difficulty == "hard":
     attempts = 5
-  
-  def check_number(number, guess):
-    if number > guess:
-      print("Too low.")
-    elif number < guess:
-      print("Too high.")
-    else:
-      print(f"You got it! The answer was {number}.")
-  
+  else:
+    print("Invalid input.")
+    return
+    
   while attempts > 0:
     print(f"You have {attempts} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
-    check_number(number, guess)
-    if number == guess:
-      attempts -= 0
-      break
-    else:
-      attempts -= 1
-  
+    attempts = check_number(number, guess, attempts)
+    # #if guess is wrong, reduce attempts by 1
+    # #if guess is correct, end the game
+    # if number == guess:
+    #   attempts -= 0
+    # else:
+    #   attempts -= 1
+  #if attempts is 0, end the game
   if attempts == 0:
     print("You've run out of guesses, you lose.")
     print(f"Pssst, the correct answer is {number}")
+    return
+  elif attempts == -1:
+    return
 
 play_game()
+while input("Type 'y' to play again or 'n' to exit: ") == "y":
+  clear()
+  play_game()
+
+print("Good Bye")
